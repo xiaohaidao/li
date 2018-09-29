d12ec11457a372d66eb9e0d9284fb94cfa143a26
li <oxox0@qq.com>
2018-09-29 22:59:19 +0800

    Update CMakeLists.txt

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,42 +1,73 @@
 
-if(CMAKE_BUILD_TYPE MATCHES "Debug")
-    set(BUILD ${PROJECT_NAME}d)
-elseif(CMAKE_BUILD_TYPE MATCHES "Release")
-    set(BUILD ${PROJECT_NAME})
-elseif(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo ")
-    set(BUILD ${PROJECT_NAME}rd)
-elseif(CMAKE_BUILD_TYPE MATCHES "MinSizeRel ")
-    set(BUILD ${PROJECT_NAME}mr)
-else()
-    set(BUILD ${PROJECT_NAME})
-endif()
+macro(AddExecute)
+    set(options)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs SOURCE QT_MODULE)
+    cmake_parse_arguments(AddExecute "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN})
 
-macro(use_cxx11)
-    if (CMAKE_VERSION VERSION_LESS "3.1")
-        if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-            set (CMAKE_CXX_FLAGS "-std=gnu++11 ${CMAKE_CXX_FLAGS}")
-        endif ()
-    else ()
-        set (CMAKE_CXX_STANDARD 11)
-    endif ()
-endmacro(use_cxx11)
+    string(LENGTH "${AddExecute_QT_MODULE}" HAVE_QT_MODULE)
+    if("${HAVE_QT_MODULE}")
+        find_package(Qt5 COMPONENTS ${AddExecute_QT_MODULE} REQUIRED)
+        if(NOT Qt5_FOUND)
+            message(FATAL_ERROR "QT not find. you can set CMAKE_PREFIX_PATH/Qt5_DIR!")
+        endif()
 
-macro(ForSubdir arg...)
-    foreach(dirs ${ARGV})
-        add_subdirectory(${dirs})
-    endforeach()
+        set(CMAKE_AUTOMOC ON)
+        set(CMAKE_AUTORCC ON)
+        set(CMAKE_AUTOUIC ON)
+        set(CMAKE_INCLUDE_CURRENT_DIR ON)
+    endif()
+    add_executable("${AddExecute_TARGET}" "${AddExecute_SOURCE}")
+
+    set_target_properties("${AddExecute_TARGET}" PROPERTIES DEBUG_POSTFIX d)
+
+    if("${HAVE_QT_MODULE}")
+        qt5_use_modules("${AddExecute_TARGET}" "${AddExecute_QT_MODULE}")
+    endif()
 endmacro()
 
-macro(Findqt5 arg...)
-    if (NOT CMAKE_PREFIX_PATH)
-        #set(CMAKE_PREFIX_PATH )
-    endif()
-    set(CMAKE_INCLUDE_CURRENT_DIR ON)
-    set(CMAKE_AUTOMOC ON)
-    set(CMAKE_AUTORCC ON)
-    find_package(Qt5 COMPONENTS ${ARGV} REQUIRED)
+function(GenerateDoc)
+    find_package(Doxygen QUIET)
+    option(BUILD_DOCUMENTATION "Create and install the HTML based API documentation (requires Doxygen)" ${DOXYGEN_FOUND})
 
-    if(NOT Qt5_FOUND)
-        message(FATAL_ERROR "QT not find. you can set CMAKE_PREFIX_PATH!")
+    if(BUILD_DOCUMENTATION)
+        if(NOT DOXYGEN_FOUND)
+            message(FATAL_ERROR "Doxygen is needed to build the documentation.")
+        endif()
+
+        # Set the Readme.md file into html
+        option(USER_DOXY_PAGE "Add the Readme.md to the main page in html what doxyen generated" false)
+        if (USER_DOXY_PAGE)
+            set(doxy_main_page Readme.md)
+        endif()
+
+        set(doxyfile_in "${CMAKE_MODULE_PATH}/template/Doxyfile.in")
+        set(doxyfile "${CMAKE_BINARY_DIR}/Doxyfile")
+
+        configure_file("${doxyfile_in}" "${doxyfile}" @ONLY)
+
+        add_custom_target(doc
+            COMMAND "${DOXYGEN_EXECUTABLE}" "${doxyfile}"
+            WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/docs"
+            COMMENT "Generating API documentation with Doxygen"
+            VERBATIM)
+
+        #install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc)
     endif()
-endmacro(Findqt5)
+endfunction()
+
+function(ADD_SUBDIRLIST)
+    set(options)
+    set(oneValueArgs CURDIR RESULT)
+    set(multiValueArgs)
+    cmake_parse_arguments(ADD_SUBDIRLIST "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN})
+
+    FILE(GLOB children RELATIVE "${ADD_SUBDIRLIST_CURDIR}" "${ADD_SUBDIRLIST_CURDIR}/*")
+    FOREACH(child ${children})
+        IF(IS_DIRECTORY "${ADD_SUBDIRLIST_CURDIR}/${child}")
+            ADD_SUBDIRECTORY("${ADD_SUBDIRLIST_CURDIR}/${child}")
+        ENDIF()
+    ENDFOREACH()
+endfunction()
